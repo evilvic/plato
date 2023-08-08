905005f077008b5210a2f11940ca3e785d3c836b
Víctor Peña Romero <vic.pero@icloud.com>
2023-08-07 18:38:42 -0600

    start with styles

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,7 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { supabase } from '@/services/supabase'
-
-import type { Ref } from 'vue'
-
-interface Country {
-  id: number
-  name: string
-}
-
-const countries: Ref<Country[]> = ref([])
-
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = data || []
-}
-
-onMounted(() => {
-  getCountries()
-})
 </script>
 
 <template>
-  <ul>
-    <li 
-      v-for="country in countries" 
-      :key="country.id"
-    >
-      {{ country.name }}
-    </li>
-  </ul>
   <RouterView />
 </template>
 
--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -3,4 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseProjectUrl = import.meta.env.VITE_SUPABASE_PROJECT_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseProjectUrl, supabaseAnonKey)
+export const supabase = createClient(
+  supabaseProjectUrl, 
+  supabaseAnonKey
+)
