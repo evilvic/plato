1a2a8a1dabc94913949ed5860def7d359c98cabd
Víctor Peña Romero <vic.pero@icloud.com>
2023-09-04 21:32:06 -0600

    button size

--- src/components/core/Navigation.vue
+++ src/components/core/Navigation.vue
@@ -1,6 +1,9 @@
 <template>
   <nav class="navigation">
-    <p>Nuevo</p>
+    <button class="navigation__el">
+      <div class="navigation__icon" />
+      <span class="navigation__topic">Nuevo</span>
+    </button>
   </nav>
 
 </template>
@@ -10,12 +13,33 @@
 
 .navigation {
   width: 100%;
-  height: 75px;
-  background-color: $bg-39;
+  height: calc(48px + var(--safe-area-inset-bottom));
+  background: $bg-19;
   position: fixed;
   bottom: 0;
-  & p {
-    color: #999999;
+  display: flex;
+  justify-content: center;
+  padding: 4px 0 0;
+  &__el {
+    width: 40px;
+    height: 44px;
+    background: transparent;
+    border: none;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    padding: 4px 0 0;
+  }
+  &__icon {
+    width: 24px;
+    height: 24px;
+    background: $bg-99;
+  }
+  &__topic {
+    font-size: 10px;
+    line-height: 10px;
+    color: $bg-99;
   }
 }
 </style>
