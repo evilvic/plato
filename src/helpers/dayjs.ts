625b569d0d537ea1482cf1f7673fb91c8d0b4116
Víctor Peña Romero <vic.pero@icloud.com>
2023-09-07 17:14:30 -0600

    test dates

--- src/helpers/dayjs.ts
+++ src/helpers/dayjs.ts
@@ -1,8 +1,29 @@
 import dayjs from 'dayjs';
+import localeData from 'dayjs/plugin/localeData'
 import utc from 'dayjs/plugin/utc';
+import 'dayjs/locale/es';
 
 dayjs.extend(utc);
+dayjs.extend(localeData)
+dayjs.locale('es');
 
 export function formatTimeToHHMM(dateString: string): string {
   return dayjs.utc(dateString).local().format('HH:mm');
+}
+
+export function formatDateToYYYYMMDD(dateString: string): string {
+  return dayjs.utc(dateString).local().format('YYYY-MM-DD');
+}
+
+export const formatDateWithTodayAndYesterday = (date: string) => {
+  const today = dayjs();
+  const inputDate = dayjs(date);
+  
+  if (inputDate.isSame(today, 'day')) {
+      return `HOY | ${inputDate.format('D [de] MMMM')}`;
+  } else if (inputDate.isSame(dayjs(today).subtract(1, 'day'), 'day')) {
+      return `AYER | ${inputDate.format('D [de] MMMM')}`;
+  } else {
+      return `${inputDate.format('dddd | D [de] MMMM')}`;
+  }
 }
--- src/views/DiaryView.vue
+++ src/views/DiaryView.vue
@@ -1,33 +1,93 @@
 <script setup lang="ts">
 import { requestHKAuthorization } from '@/plugins/HealthKitPlugin'
 import { queryData } from '@/plugins/HealthKitPlugin'
-import { formatTimeToHHMM } from '@/helpers/dayjs'
-import { ref, onMounted } from 'vue'
+import { formatTimeToHHMM, formatDateToYYYYMMDD, formatDateWithTodayAndYesterday } from '@/helpers/dayjs'
+import { ref, computed, onMounted } from 'vue'
 
-const loading = ref(false);
-const data = ref<any[]>([]);
+interface WaterData {
+  value: number;
+  source: string;
+  unitName: string;
+  uuid: string;
+  endDate: string;
+  duration: number;
+  sourceBundleId: string;
+  startDate: string;
+}
+
+const loading = ref<boolean>(false);
+const data = ref<Record<string, WaterData[]>>({});
+
+const castedData = computed(() => data.value as Record<string, WaterData[]>);
 
 onMounted(() => {
-  requestHKAuthorization()
+  // requestHKAuthorization()
   getWaterIntake();
-  // data.value = [
-  // {
-  //   "value":372,
-  //   "source":"Health",
-  //   "unitName":"milliliter",
-  //   "uuid":"25024F31-3517-42BF-ADE8-72FFDF74D447",
-  //   "endDate":"2023-08-31T18:16:00Z",
-  //   "duration":0,
-  //   "sourceBundleId":"com.apple.Health",
-  //   "startDate":"2023-08-31T18:16:00Z"
-  // }
-  // ]
+  // getHarcodedWater();
 })
 
 const getWaterIntake = async () => {
-  const water = await queryData();
-  data.value = water || [];
+  const water: WaterData[] = await queryData() || [];
+  data.value = groupByDate(water);
   loading.value = false;
+}
+
+const getHarcodedWater = () => {
+  const water = [
+    {
+      "value":400,
+      "source":"Health",
+      "unitName":"milliliter",
+      "uuid":"25024F31-3517-42BF-ADE8-72FFDF74D447",
+      "endDate":"2023-08-31T18:16:00Z",
+      "duration":0,
+      "sourceBundleId":"com.apple.Health",
+      "startDate":"2023-09-07T18:16:00Z"
+    },
+    {
+      "value":200,
+      "source":"Health",
+      "unitName":"milliliter",
+      "uuid":"25024F31-3517-42BF-ADE8-72FFDF74D447",
+      "endDate":"2023-08-31T18:16:00Z",
+      "duration":0,
+      "sourceBundleId":"com.apple.Health",
+      "startDate":"2023-09-05T18:16:00Z"
+    },
+    {
+      "value":350,
+      "source":"Health",
+      "unitName":"milliliter",
+      "uuid":"25024F31-3517-42BF-ADE8-72FFDF74D447",
+      "endDate":"2023-08-31T18:16:00Z",
+      "duration":0,
+      "sourceBundleId":"com.apple.Health",
+      "startDate":"2023-09-06T18:16:00Z"
+    },
+  ]
+
+  data.value = groupByDate(water);
+}
+
+const groupByDate = (waterData: WaterData[]): Record<string, WaterData[]> => {
+  const grouped: Record<string, WaterData[]> = {};
+
+  waterData.forEach(item => {
+    const dateKey = formatDateToYYYYMMDD(item.startDate);
+    if (!grouped[dateKey]) {
+      grouped[dateKey] = [];
+    }
+    grouped[dateKey].push(item);
+  });
+
+  const sortedDates = Object.keys(grouped).sort((a, b) => new Date(b).getTime() - new Date(a).getTime());
+  
+  const orderedData: Record<string, WaterData[]> = {};
+  sortedDates.forEach(date => {
+    orderedData[date] = grouped[date];
+  });
+
+  return orderedData;
 }
 /*
 {
@@ -45,23 +105,23 @@ const getWaterIntake = async () => {
 
 <template>
   <main>
-    <ul
-      class="container"
-      v-if="!loading && data"
-    >
-      <li
-        class="card"
-        v-for="el in data"
-        :key="el.uuid"
+    <div v-for="(entries, date) in castedData" :key="date">
+      <h3>  {{ formatDateWithTodayAndYesterday(date) }} </h3>
+      <ul
+        class="container"
+        v-if="!loading && data"
       >
-        <span class="card__qty">{{ el.value }} ml</span>
-        <span class="card__bottom">
-          {{ formatTimeToHHMM(el.startDate) }}
-        </span>
-      </li>
-    </ul>
-    <div v-else>
-      loading...
+        <li
+          class="card"
+          v-for="entry in entries"
+          :key="entry.uuid"
+        >
+          <span class="card__qty">{{ entry.value }} ml</span>
+          <span class="card__bottom">
+            {{ formatTimeToHHMM(entry.startDate) }}
+          </span>
+        </li>
+      </ul>
     </div>
   </main>
 </template>
@@ -72,6 +132,12 @@ const getWaterIntake = async () => {
 
 main {
   padding: 0 16px;
+}
+h3 {
+  margin: 12px 0 6px;
+}
+h3::first-letter {
+    text-transform: capitalize;
 }
 .container {
   display: flex;
