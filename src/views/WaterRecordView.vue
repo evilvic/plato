94a249cf8bc7d21752636eef5339e5fc2df75163
Víctor Peña Romero <vic.pero@icloud.com>
2023-09-22 12:30:02 -0600

    solv env + add water record view

---
+++ src/views/WaterRecordView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const water = ref<number>(0)
+
+const handleSave = () => {
+  console.log(water.value)
+}
+
+</script>
+
+<template>
+  <div class="water-record">
+    <input
+      type="number"
+      placeholder="0"
+      v-model="water"
+    >
+    <button
+      @click="handleSave"
+    >
+      Guardar
+    </button>
+  </div>
+</template>
